879a9e3bfad9a51517b7724d07b861abc8dfb841
xXxEdyxXx <adam.stupka@gmail.com>
2025-04-23 11:45:16 +0200

    PenaltyPointsFormating

--- commands/penaltyPoints.py
+++ commands/penaltyPoints.py
@@ -66,7 +66,7 @@ async def penalty_points(ctx, league: str = None):
                     color=discord.Color.red()
                 )
                 for _, row in df.iterrows():
-                    embed.add_field(name=row["Driver"], value=f"{row['Points']} points", inline=False)
+                    embed.add_field(name=row["Driver"], value=f"{row['Points']} points", inline=True)
                     if row["Points"] >= 12:
                         messages.append(f"🚨 {lg}: **{row['Driver']}** SHOULD BE BANNED FOR THIS WEEK!")
 
