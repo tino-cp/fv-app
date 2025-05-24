e73a8ff1aa1eb50dc79f842bb5e539ca8af66291
xXxEdyxXx <adam.stupka@gmail.com>
2025-05-24 09:34:10 +0200

    Teamprincipals fixed

--- commands/protest.py
+++ commands/protest.py
@@ -96,12 +96,12 @@ async def protest_command(ctx, team: str):
         await ctx.send("❌ This command is only allowed on the Formula V or test servers.")
         return
 
-    allowed_roles = {"Academy CEO", "F2 Team Manager ", "F1 Team Principal", "Academy Manager"}
+    allowed_roles = {"Academy CEO", "F2 Team Principal", "F3 Team Principal", "Academy Manager"}
     user_roles = {role.name for role in ctx.author.roles}
 
     if not allowed_roles.intersection(user_roles):
         embed = discord.Embed(
-            description="❌ You do not have permission to use this command. Only Academy Manager, F2 Team Manager and F1 Team Principal can submit protests.\n\n Ping and ask your manager to open a protest for you.",
+            description="❌ You do not have permission to use this command. Only Academy Manager, F2 Team Principal and F3 Team Principal can submit protests.\n\n Ping and ask your manager to open a protest for you.",
             color=discord.Color.red()
         )
         await ctx.send(embed=embed)
